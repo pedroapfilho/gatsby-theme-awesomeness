eade11e8b491b7192333cc1ac939b2ff706d5068
Pedro Filho <pedro@filho.me>
2019-10-18 14:34:54 -0300

    adding source filesystem

--- example/gatsby-config.js
+++ example/gatsby-config.js
@@ -1,3 +1,12 @@
 module.exports = {
-  plugins: ["gatsby-theme-awesomeness"]
+  plugins: [
+    `gatsby-theme-awesomeness`,
+    {
+      resolve: `gatsby-source-filesystem`,
+      options: {
+        name: `images`,
+        path: `${__dirname}/src/images/`,
+      },
+    },
+  ]
 }
--- example/src/pages/index.tsx
+++ example/src/pages/index.tsx
@@ -1,6 +1,8 @@
 import React, { FC } from "react"
 import styled from "styled-components"
 import Helmet from "react-helmet"
+import { graphql } from "gatsby"
+import Img from "gatsby-image"
 
 const StyledTitle = styled.h1`
   color: red;
@@ -9,7 +11,20 @@ const Index: FC<{data: any}> = ({data}) => (
   <>
     <Helmet title="foo" defer={false} />
     <StyledTitle>test</StyledTitle>
+    <Img fixed={data.file.childImageSharp.fixed} />
   </>
 )
+
+export const query = graphql`
+  query {
+    file(relativePath: { eq: "150.png" }) {
+      childImageSharp {
+        fixed(width: 150, height: 150) {
+          ...GatsbyImageSharpFixed
+        }
+      }
+    }
+  }
+`
 
 export default Index
