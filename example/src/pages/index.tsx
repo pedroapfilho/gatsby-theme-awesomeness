e2e5a30d1c722b26aeb8548ca3982533b543a575
Pedro Filho <pedro@filho.me>
2019-11-11 22:26:03 -0300

    fixing example index

--- example/src/pages/index.tsx
+++ example/src/pages/index.tsx
@@ -1,31 +1,33 @@
 import React, { FC } from "react";
 import styled from "styled-components";
 import Helmet from "react-helmet";
-import { graphql } from "gatsby";
 import Img from "gatsby-image";
+import { graphql, useStaticQuery } from "gatsby";
 
 const StyledTitle = styled.h1`
   color: red;
 `;
 
-const Index: FC<{ data: any }> = ({ data }) => (
-  <>
-    <Helmet title="foo" defer={false} />
-    <StyledTitle>test</StyledTitle>
-    <Img fixed={data.file.childImageSharp.fixed} />
-  </>
-);
-
-export const query = graphql`
-  query {
-    file(relativePath: { eq: "150.png" }) {
-      childImageSharp {
-        fixed(width: 150, height: 150) {
-          ...GatsbyImageSharpFixed
+const Index: FC = () => {
+  const data = useStaticQuery(graphql`
+    query {
+      file(relativePath: { eq: "150.png" }) {
+        childImageSharp {
+          fixed(width: 150, height: 150, quality: 100) {
+            ...GatsbyImageSharpFixed
+          }
         }
       }
     }
-  }
-`;
+  `);
+
+  return (
+    <>
+      <Helmet title="foo" defer={false} />
+      <StyledTitle>test</StyledTitle>
+      <Img fixed={data.file.childImageSharp.fixed} />
+    </>
+  );
+};
 
 export default Index;
