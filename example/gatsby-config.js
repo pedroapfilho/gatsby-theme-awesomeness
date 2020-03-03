f59567cf085cd9859d13c0c3efaa3cc9d8d5139c
Pedro Filho <pedro@filho.me>
2020-03-03 10:09:35 +0000

    upgrading XO

--- example/gatsby-config.js
+++ example/gatsby-config.js
@@ -1,12 +1,12 @@
 module.exports = {
-  plugins: [
-    {
-      resolve: `gatsby-source-filesystem`,
-      options: {
-        name: `images`,
-        path: `${__dirname}/src/images/`
-      }
-    },
-    `gatsby-theme-awesomeness`
-  ]
+	plugins: [
+		{
+			resolve: 'gatsby-source-filesystem',
+			options: {
+				name: 'images',
+				path: `${__dirname}/src/images/`
+			}
+		},
+		'gatsby-theme-awesomeness'
+	]
 };
--- gatsby-theme-awesomeness/gatsby-config.js
+++ gatsby-theme-awesomeness/gatsby-config.js
@@ -1,17 +1,17 @@
 module.exports = {
-  plugins: [
-    `gatsby-plugin-typescript`,
-    `gatsby-plugin-styled-components`,
-    `gatsby-plugin-react-helmet`,
-    `gatsby-plugin-sharp`,
-    `gatsby-transformer-sharp`,
-    {
-      resolve: `gatsby-plugin-robots-txt`,
-      options: {
-        policy: [{ userAgent: `*`, allow: `/` }]
-      }
-    },
-    `gatsby-plugin-sitemap`,
-    `gatsby-plugin-offline`
-  ]
+	plugins: [
+		'gatsby-plugin-typescript',
+		'gatsby-plugin-styled-components',
+		'gatsby-plugin-react-helmet',
+		'gatsby-plugin-sharp',
+		'gatsby-transformer-sharp',
+		{
+			resolve: 'gatsby-plugin-robots-txt',
+			options: {
+				policy: [{userAgent: '*', allow: '/'}]
+			}
+		},
+		'gatsby-plugin-sitemap',
+		'gatsby-plugin-offline'
+	]
 };
